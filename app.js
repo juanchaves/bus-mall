670fa4a2b1f218fcfe90a4e5e72aea479de6f48d
juanchaves <chavesjuanm@gmail.com>
2016-12-13 14:16:58 -0800

    trying to randomize images v0.2

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict'
 
 //Global variables & array here:
+var imgContainer = document.getElementById('image_container');
+var leftImg = document.getElementById('left');
+var centerImg = document.getElementById('center');
+var rightImg = document.getElementById('right');
+var previousArray = [];
+var currentArray = [];
 var allProducts =[];
-var imgName = ['bag','bannana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+var imgName = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 
 //Constructor
 
@@ -19,26 +25,24 @@ for (var i = 0; i < imgName.length; i++) {
   new Product(imgName[i]);
 }
 //Functions
-///Event Handler
-////Random Image Generator
-var leftImg = document.getElementById('left');
-var centerImg = document.getElementById('center');
-var rightImg = document.getElementById('right');
 
-function getRandomImg() {
-  for (var i = 0; i < 3; i++) {
-    Math.floor(Math.random()* imgName.length);
-    return
-  }
-  var img = allProducts[num].imgPath;
-  leftImg.src = img;
-  centerImg.src = img;
-  rightImg.src =img;
+function getRandomNum() {
+  return Math.floor(Math.random() * imgName.length);
+}
+currentArray.push(getRandomNum());
+currentArray.push(getRandomNum());
+console.log(currentArray);
+
+// if
+
+function displayLeft() {
+  leftImg.src = allProducts[currentArray[0]].imgPath;
 }
 
-getRandomImg();
+displayLeft();
 
-
+///Event Handler
+////Random Image Generator
 
 
 ////Event Listener
